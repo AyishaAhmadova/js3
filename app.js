0c2f72ba0cb8cc4e9c1d767f2c3b0132a326bcea
ayishaahmadova <asgarlia29@gmail.com>
2024-04-02 18:59:05 +0400

    js3

---
+++ app.js
@@ -0,0 +1,81 @@
+// let customer={
+//     "name":"Cavid",
+//     "surname" : "Memmedli",
+//     "age": "18"
+// };
+// console.log( customer);
+
+// let customera={
+//     "name":"Rza",
+//     "surname" : "Memmedov",
+//     "age": "18"
+// };
+
+// let customerb={
+//     "name":"Aisha",
+//     "surname" : "Ahmadova",
+//     "age": "26"
+// };
+
+// let customerc={
+//     "name":"Ilaha",
+//     "surname" : "Asgarli",
+//     "age": "25"
+// };
+
+// let customers=[
+//     customera,
+//     customerb,
+//     customerc
+// ]
+
+// console.log( customers);
+
+// let students = [
+//   {
+//     name: "Cavid",
+//     surname: "Memmedov",
+//     mark: 78,
+//     hobbies: ["football", "walking", "swimming"],
+//   },
+//   {
+//     name: "Nofel",
+//     surname: "ibrahimov",
+//     mark: 100,
+//     hobbies: ["football", "play the piano", "read novel"],
+//   },
+//   {
+//     name: "Ilkin",
+//     surname: "Safarov",
+//     mark: 12,
+//     hobbies: ["play the piano", "watching the film", "walking"],
+//   },
+//   {
+//     name: "Nermin",
+//     surname: "Aliyeva",
+//     mark: 51,
+//     hobbies: ["read novel", "camp", "play the guitar"],
+//   },
+// ];
+// console.log(students[0].hobbies);
+// console.log(students.map(student => student.name));
+//let lowMark=students.filter(studnt =>students.mark<=90);
+//lowMark.forEach(s=>console.log("xeberdarliq edin: ${s.name} ${s.soyad} ${s.mark}"));
+
+// let cem=0
+// students.map(student=>cem +=student.mark);
+// let average=cem/students.length
+// console.log(average);
+
+let data = prompt("Hello");
+let length = data.length;
+
+for (let i = 0; i < length * 2 - 1; i++) {
+  let soz = "";
+  if (i < length) {
+    soz = data.substring(0, i + 1);
+  } else {
+    soz = data.substring(0, length - (i - length + 1));
+  }
+  console.log(soz);
+}
